3adc6f92fe9337cabbbd44e1f1127a4733b05290
mina <mi.ajorlou@gmail.com>
2019-04-30 16:23:46 +0430

    Initial commit

---
+++ app/src/main/java/malltina/com/kotlin/MainActivity.kt
@@ -0,0 +1,12 @@
+package malltina.com.kotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
